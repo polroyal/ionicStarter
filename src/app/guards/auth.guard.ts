7888ef50edd9bb748d1235632f6e6599e375d483
polycarp <polycarproyal@gmail.com>
2020-10-08 14:00:47 +0300

    done

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,29 @@
+import { AuthenticationService } from './../services/authentication.service';
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { filter, map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+
+  constructor(private authService: AuthenticationService, private router: Router) { }
+
+  canLoad(): Observable<boolean> {
+    return this.authService.isAuthenticated.pipe(
+      filter(val => val !== null), // filter out initial Behavior subject value
+      take(1), //Otherwise the Observable doesn't complete
+      map(isAuthenticated => {
+        if (isAuthenticated) {
+          return true;
+        } else {
+          this.router.navigateByUrl('/login');
+          return false;
+        }
+      })
+     );
+    }
   }
-}
+
--- src/app/guards/auto-login.guard.ts
+++ src/app/guards/auto-login.guard.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from './../services/authentication.service';
+import { filter, map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutoLoginGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+
+  constructor(private authService: AuthenticationService, private router: Router) { }
+  canLoad(): Observable<boolean> {
+    return this.authService.isAuthenticated.pipe(
+      filter(val => val !== null),// Filter out initial Behavious subject value
+      take(1), // Otherwise the Observable doesn't complete
+      map(isAuthenticated => {
+        console.log('Found previous token, automatic login');
+        if (isAuthenticated) {
+          //Directly open inside area
+          this.router.navigateByUrl('/tabs', { replaceUrl: true });
+        } else {
+          //simply allow access to the login 
+          return true;
+        }
+      })
+    );
   }
 }
